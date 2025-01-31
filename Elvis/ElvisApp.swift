f54980e49a459026df460ec74046d1202acf8b89
dljardim <dljardim@outlook.com>
2025-01-31 13:42:51 -0600

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Damian Jardim on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
